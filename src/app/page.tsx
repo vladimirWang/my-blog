3f6f0f11958bef5ddfcd978769f03e53861680d5
wangxiaoyu006 <wangxiaoyu006@chinasofti.com>
2024-10-03 15:36:29 +0800

    初始化shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import LatestPosts from "@/components/home/latest-posts";
+import MainNav from "@/components/ui/main-nav";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <LatestPosts />
+      <MainNav />
+      <main>
+        <div>
+          <LatestPosts />
+        </div>
+      </main>
     </>
   );
 }
