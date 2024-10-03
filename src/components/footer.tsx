15a29393d8113cffbc1b813f6fee58bed0de04b6
wangxiaoyu006 <wangxiaoyu006@chinasofti.com>
2024-10-03 22:52:22 +0800

    完成footer组件

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Icons } from "./icons";
 import { POSTS } from "@/lib/constants";
 import Link from "next/link";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 export default function Footer() {
   return (
@@ -50,6 +52,55 @@ export default function Footer() {
               ))}
             </ul>
           </div>
+          <div className="space-y-4">
+            <h3 className="text-md font-semibold">Links</h3>
+            <ul className="space-y-2 text-sm">
+              <li>
+                <a
+                  href="mailto:413114463@qq.com"
+                  className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+                >
+                  Contact
+                </a>
+              </li>
+              <li>
+                <Link
+                  href={"/terms-of-services"}
+                  className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+                >
+                  Terms of Services
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={"/privacy-policy"}
+                  className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+                >
+                  Privacy Policy
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-4">
+            <h3 className="text-md font-semibold">Newsletter</h3>
+            <p className="text-gray-500 dark:text-gray-400 text-sm">
+              Subscribe to our newsletter to stay up-to-date with the latest
+              news and updates.
+            </p>
+            <form>
+              <div className="flex space-x-2">
+                <Input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="flex-1"
+                />
+                <Button>Subscribe</Button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="mt-8 border-t border-gray-200 pt-4 text-center text-xs text-gray-500 dark:border-gray-700 dark:text-gray-400">
+          &copy; 2024 Coding Fernando. All rights reserved.
         </div>
       </div>
     </footer>
